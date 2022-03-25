e44f72d42ca402fcebb2340608165d47f9aff24b
Raul <raulirawan33@gmail.com>
2022-03-25 09:20:07 +0700

    api gambar

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    public function image(Request $request)
+    {
+        $header = $request->header()['authorization'][0];
+        $header = base64_decode($header);
+        if($header == 'main_panel:0p3n_th!sR0ute') {
+            $url = json_decode($request->data);
+            $tujuan_upload = $url->storage;
+            if($request->hasFile('file')) {
+                $file = $request->file('file');
+                // isi dengan nama folder tempat kemana file diupload
+                $nama_file = $url->nama_file;
+                $file->move($tujuan_upload,$nama_file);
+
+                $fileLama = $tujuan_upload.'/'.$url->url_file;
+                if(file_exists($fileLama)) {
+                    unlink($fileLama);
+                  }
+
+            }
+            if($request->hasFile('file2')) {
+                $file2 = $request->file('file2');
+                // isi dengan nama folder tempat kemana file diupload
+
+                $nama_file = $url->nama_file2;
+                $file2->move($tujuan_upload,$nama_file);
+
+                $fileLama = $tujuan_upload.'/'.$url->url_file2;
+                if(file_exists($fileLama)) {
+                    unlink($fileLama);
+                  }
+
+            }
+            return [
+                'code'      => 200,
+    			'response' => true,
+    			'message' => 'success'
+    		];
+        } else {
+            return [
+                'code'      => 200,
+    			'response' => false,
+    			'message' => 'authorization is fail!'
+    		];
+        }
+
+
+
+    }
+
+    public function hapusQr(Request $request)
+    {
+        $header = $request->header()['authorization'][0];
+        $header = base64_decode($header);
+        $fileLama = $request->nama_file;
+        if($header == 'main_panel:0p3n_th!sR0ute') {
+            if(file_exists($fileLama)) {
+                unlink($fileLama);
+              }
+            return [
+                'code'      => 200,
+    			'response' => true,
+    			'message' => 'success'
+    		];
+        }else {
+            return [
+                'code'      => 200,
+    			'response' => false,
+    			'message' => 'authorization is fail!'
+    		];
+        }
+
+
+
+    }
+
+}
